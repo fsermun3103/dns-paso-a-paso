6644dbeec0b776878a8cded379b179e8dd0001bd
fsermun3103 <fsermun3103@ieszaidinvergeles.org>
2025-10-15 22:56:29 +0200

    Ajustes bootsrap.sh y Vagrantfile

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,2 +1,16 @@
-cp /home/vagrant/password.txt /vagrant/config/
-cp /vagrant/config/password.txt /home/vagrant/
+sudo apt update
+sudo apt -y upgrade
+
+sudo apt -y install bind9 bind9utils bind9-doc dnsutils
+
+cp /vagrant/config/named.conf.options /etc/bind/
+cp /vagrant/config/named.conf.local /etc/bind/
+cp /vagrant/config/paco.test.dns /var/lib/bind/
+cp /vagrant/config/paco.test.rev /var/lib/bind/
+
+sudo chown bind:bind /var/lib/bind/paco.test.dns
+sudo chown bind:bind /var/lib/bind/paco.test.rev
+sudo chmod 644 /var/lib/bind/paco.test.dns
+sudo chmod 644 /var/lib/bind/paco.test.rev
+
+sudo systemctl restart bind9
